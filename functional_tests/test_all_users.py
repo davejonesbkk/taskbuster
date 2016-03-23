d7f2f13fc9b694a294229aa1e65fa4a8890a044a
David Jones <davejonesbkk@gmail.com>
2016-03-23 08:58:01 +0700

    Further changes for part V

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -5,23 +5,32 @@ from selenium import webdriver
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.translation import activate
+import time
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(6)
+		self.browser.implicitly_wait(5)
 		activate('en')
+
 
 	def tearDown(self):
 		self.browser.quit()
 
+	
 	def get_full_url(self, namespace):
 		return self.live_server_url + reverse(namespace)
 
+		#time.sleep(5)
+
+
 	def test_home_title(self):
 		self.browser.get(self.get_full_url("home"))
-		self.assertIn("Taskbuster", self.browser.title)
+		self.assertIn("TaskBuster", self.browser.title)
+
+		#time.sleep(5)
+
 
 	def test_h1_css(self):
 		self.browser.get(self.get_full_url("home"))
@@ -29,19 +38,31 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
 		self.assertEqual(h1.value_of_css_property("color"),
 					"rgba(200, 50, 255, 1)")
 
+		#time.sleep(5)
+
+	
+
 	def test_home_files(self):
 		self.browser.get(self.live_server_url + "/robots.txt")
 		self.assertNotIn("Not Found", self.browser.title)
 		self.browser.get(self.live_server_url + "/humans.txt")
 		self.assertNotIn("Not Found", self.browser.title)
+
+		#time.sleep(5)
+
+
 	
 	def test_internationalization(self):
-		for lang, h1_text in [('en', 'Welcome to Taskbuster!'),
-							('ca', 'Benvingut a Taskbuster!')]:
+		for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+							('ca', 'Benvingut a TaskBuster!')]:
 			activate(lang)
 			self.browser.get(self.get_full_url("home"))
 			h1 = self.browser.find_element_by_tag_name("h1")
 			self.assertEqual(h1.text, h1_text)
+
+			#time.sleep(5)
+
+		
 	
 
 
--- taskbuster/test.py
+++ taskbuster/test.py
@@ -13,9 +13,11 @@ class TestHomePage(TestCase):
 		response = self.client.get(reverse("home"))
 		self.assertTemplateUsed(response, "taskbuster/index.html")
 
+
 	def test_uses_base_template(self):
 		activate('en')
 		
 		response = self.client.get(reverse("home"))
 		self.assertTemplateUsed(response, "base.html")
+
 		
