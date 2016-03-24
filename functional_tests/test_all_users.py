f94889e57dc55663714278df9f39af8e23fe57fd
David Jones <davejonesbkk@gmail.com>
2016-03-24 09:15:37 +0700

    Internationalization and localization

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -6,6 +6,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.translation import activate
 import time
+from datetime import date
+from django.utils import formats
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
 
@@ -62,8 +64,26 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
 
 			#time.sleep(5)
 
+	def test_localization(self):
+		today = date.today()
+		for lang in ['en', 'ca']:
+			activate(lang)
+			self.browser.get(self.get_full_url("home"))
+			local_date = self.browser.find_element_by_id("local-date")
+			non_local_date = self.browser.find_element_by_id("non-local-date")
+			self.assertEqual(formats.date_format(today, use_l10n=True),
+								local_date.text)
+			self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+
 		
-	
+	def test_time_zone(self):
+		self.browser.get(self.get_full_url('home'))
+		tz = self.browser.find_element_by_id('time-tz').text
+		utc = self.browser.find_element_by_id('time-utc').text
+		ny = self.browser.find_element_by_id('time-ny').text
+		self.assertNotEqual(tz, utc)
+		self.assertNotIn(ny, [tz, utc])
+		
 
 
 
